d9a2ddf3b463d370e9d369f10f8d1310b6750d71
Memtet12 <eldotefc@gmail.com>
2024-10-03 22:02:41 +0500

    task12

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -1,6 +1,7 @@
 package com.example.task12;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Task12 {
 
@@ -17,14 +18,13 @@ public class Task12 {
         return sum;
         */
 
-        return BigDecimal.ZERO;
+        return BigDecimal.valueOf(sum.floatValue() * Math.pow(1.0 + percent.floatValue(), 12)).setScale(9, RoundingMode.HALF_UP);
     }
 
     public static void main(String[] args) {
 
         BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
         BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
-
         sum = benefit(sum, percent);
 
         System.out.println("Сумма на счете через год: " + sum);
